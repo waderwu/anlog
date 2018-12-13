5a33037dd4394a174c2a740e50c48a42660862f8
Sparknack <839414890@qq.com>
2018-12-13 23:40:35 +0800

    change some contents in front end

--- log/views.py
+++ log/views.py
@@ -6,6 +6,9 @@ from django.db.models import Q
 from .models import Log
 from django.db.models import Count
 from datetime import timedelta
+import base64
+import json
+import ast
 from datetime import datetime
 import pytz
 from datetime import time
@@ -16,6 +19,8 @@ import numpy as np
 
 def index(requests):
     log_list = Log.objects.all()
+
+    # IP statistic
     ip_addrs = set(log_list.values_list("attackip"))
     ip_info = {}
     for ip in ip_addrs:
@@ -24,6 +29,7 @@ def index(requests):
         ip_info[ip[0]]['visit_num'] = len(visited_log)
         ip_info[ip[0]]['attack_num'] = len(visited_log.exclude(attacktype="[]"))
 
+    # attacking number statistic
     log_sorted_by_time = log_list.order_by("-attacktime")
     previous_min = log_sorted_by_time[0].attacktime.replace(second=0)
     after_min = previous_min + timedelta(minutes=1)
@@ -34,26 +40,6 @@ def index(requests):
         min_point.append(str(previous_min))
         after_min = previous_min
         previous_min = previous_min - timedelta(minutes=1)
-    # foremost_min = latest_min - timedelta(minutes=10)
-    # foremost_min = foremost_min.astimezone(pytz.utc)
-    # after_min = latest_min + timedelta(minutes=1)
-    # previous_min = latest_min
-    # visit_nums = []
-    # cnt = 0
-    # idx = 0
-    # min_stamp = []
-    # previous_min = previous_min.astimezone(pytz.utc)
-    # print(type(previous_min))
-    # while previous_min >= foremost_min:
-    #     if log_sorted_by_time[idx].attacktime > previous_min:
-    #         cnt += 1
-    #         idx += 1
-    #     else:
-    #         previous_min = previous_min - timedelta(minutes=1)
-    #         visit_nums.append(cnt)
-    #         print(cnt)
-    #         min_stamp.append(str(previous_min))
-    #         cnt = 0
 
     return render(requests, "index.html",
                   {'ip_info': ip_info, 'visit_nums': visit_nums, 'min_stamp': min_point})
@@ -71,13 +57,6 @@ def replay(requests):
 
 def show(requests):
     log_list = Log.objects.all()
-    ip_addrs = set(log_list.values_list("attackip"))
-    ip_info = {}
-    for ip in ip_addrs:
-        ip_info[ip[0]] = {}
-        visited_log = log_list.filter(attackip=ip[0])
-        ip_info[ip[0]]['visit_num'] = len(visited_log)
-        ip_info[ip[0]]['attack_num'] = len(visited_log.exclude(attacktype="[]"))
 
     paginator = Paginator(log_list, 20)
 
@@ -98,6 +77,8 @@ def show(requests):
     page_info['next_page_number'] = logs.next_page_number
 
     dicts = []
+    textchars = bytearray({7, 8, 9, 10, 12, 13, 27} | set(range(0x20, 0x100)) - {0x7f})
+    is_binary_string = lambda bytes: bool(bytes.translate(None, textchars))
     for log in logs:
         item = {}
         item['index'] = log.pk
@@ -106,11 +87,20 @@ def show(requests):
         item['method'] = log.method
         item['path'] = log.path
         item['headers'] = log.headers
-        item['post'] = log.post
-        item['get'] = log.get
+        item['post'] = log.post.strip('[').strip(']')
+        item['get'] = log.get.strip('[').strip(']')
         item['response'] = log.response
-        item['file'] = log.file
-        item['attacktype'] = log.attacktype
+        if log.file != '[]':
+            file_json = ast.literal_eval(log.file.strip('[').strip(']'))
+            filetype = is_binary_string(base64.b64decode(file_json['content']))
+            item['file'] = {}
+            item['file']['name'] = file_json['name']
+            item['file']['filename'] = file_json['filename']
+            item['file']['content'] = base64.b64decode(file_json['content'])
+            item['file']['type'] = filetype
+        else:
+            item['file'] = None
+        item['attacktype'] = log.attacktype.strip('[').strip(']').replace(' ', '').replace("\'", '').split(',')
         item['success'] = log.success
         dicts.append(item)
 
@@ -184,11 +174,12 @@ def search(requests):
         item['method'] = log.method
         item['path'] = log.path
         item['headers'] = log.headers
-        item['post'] = log.post
-        item['get'] = log.get
+        item['post'] = log.post.strip('[').strip(']')
+        item['get'] = log.get.strip('[').strip(']')
         item['response'] = log.response
-        item['attacktype'] = log.attacktype
-        # item['pageHtml'] = mark_safe(log.response)
+        item['file'] = log.file.strip('[').strip(']')
+        item['attacktype'] = log.attacktype.strip('[').strip(']').replace(' ', '').replace("\'", '').split(',')
+        item['success'] = log.success
         dicts.append(item)
 
     return render(requests, "temp.html", {'contents': dicts, 'page_info': page_info})
